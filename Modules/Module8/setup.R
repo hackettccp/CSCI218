b1acaccf6364d22e11555e7d16837cabaefeee77
Michael Hackett <34200282+hackettccp@users.noreply.github.com>
2021-03-02 02:40:04 -0500

    Initial import

---
+++ Modules/Module8/setup.R
@@ -0,0 +1,6 @@
+#Loading libraries and data source
+library(tidyverse)
+library(readr)
+winedata <- read.csv("winequality-red.csv")
+vgdata <- read.csv("vgsales.csv")
+disneydata <- read.csv("disney.csv")
